21093eeacbd20d3ac7eacff58eb41d936228c775
sieuneee <kseun021@naver.com>
2021-10-09 21:20:07 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const UserStorage = require("../../models/UserStorage");
+
 const output={
     home : (req, res)=>{
         res.render("home/index")
@@ -9,28 +11,25 @@ const output={
     },
 };
 
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-};
-
 const process={
     login: (req, res)=>{
         const id = req.body.id,
             psword = req.body.psword;
 
+        const users = UserStorage.getUsers("id", "psword");
+
+        const response = {};
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.psword[idx]===psword){
-                return res.json({
-                    sucess:true,
-                });
+                response.sucess = true;
+                return res.json(response);
             }
         }
-        return res.json({
-            sucess: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+
+        response.sucess = false;
+        response.msg = "로그인에 실패하셨습니다.";
+        return res.json(response);
     },
 };
 
