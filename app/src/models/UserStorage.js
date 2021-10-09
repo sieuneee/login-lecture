340fb864785ffe1396d8311418e49f05c04bd2c3
sieuneee <kseun021@naver.com>
2021-10-09 21:47:21 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,6 +17,18 @@ class UserStorage{
         }, {});
         return newUsers;
     }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userskeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = userskeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo
+    }
 }
 
 module.exports = UserStorage;
