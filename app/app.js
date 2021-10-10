cc268aa6ac1f3aedbdc142af1a15d28a7ea4d800
sieuneee <kseun021@naver.com>
2021-10-10 15:47:42 +0900

    morgan으로 로그 관리

--- app/app.js
+++ app/app.js
@@ -3,9 +3,12 @@
 //모듈
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config(); //환경변수 관리 (OS에 상관없이 환경변수 등록, 관리 가능하게 함)
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config(); //환경변수 관리 (OS에 상관없이 환경변수 등록, 관리 가능하게 함)
+
+const accessLogStream = require("./src/config/log");
 
 //라우팅
 const home = require("./src/routes/home");
@@ -17,6 +20,8 @@ app.use(express.static(`${__dirname}/src/public`));
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(morgan("dev"));
+app.use(morgan("common", {stream: accessLogStream})); //로그 저장
 
 app.use("/", home); // use : 미들 웨어를 등록해주는 메서드
 
